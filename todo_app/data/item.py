66cb27bf07dfc60dbd6f76b12f8dc133a9aea404
Agathiyan Bragadeesh <Agi.Bragadeesh@softwire.com>
2022-07-12 14:42:56 +0100

    Using datetime object in Item class

--- todo_app/data/item.py
+++ todo_app/data/item.py
@@ -1,3 +1,7 @@
+from dateutil import parser
+from datetime import datetime
+
+
 class Item:
     def __init__(self, item_id, title, desc, due_date, status='Not Started'):
         self.id = item_id
@@ -5,10 +9,11 @@ class Item:
         self.status = status
         self.desc = desc
         self.due_date = due_date
+        self.due_date_string = due_date.strftime("%m/%d/%Y") if due_date else "No due date"
 
     @classmethod
     def from_trello_card(cls, card, list_name):
         # Taking the first 10 characters of 'due' is a result of the ISO 8601 datetime return of the Trello API
-        due_date = card['due'][:10] if card['due'] else "None"
+        due_date = parser.parse(card['due']) if card['due'] else None
         desc = card['desc'] if len(card['desc']) > 0 else "No description"
         return Item(card['id'], card['name'], desc, due_date, list_name)

cb06a54f68fba79eb4c6423dae48de40d3956c82
Agathiyan Bragadeesh <Agi.Bragadeesh@softwire.com>
2022-07-13 10:39:38 +0100

    Refactored properties

--- todo_app/models/index_model.py
+++ todo_app/models/index_model.py
@@ -11,12 +11,15 @@ class IndexModel:
 
     @property
     def doing_items(self):
-        return [item for item in self._items if item.status == "Doing"]
+        return self._find_items_by_status("Doing")
 
     @property
     def to_do_items(self):
-        return [item for item in self._items if item.status == "To Do"]
+        return self._find_items_by_status("To Do")
 
     @property
     def done_items(self):
-        return [item for item in self._items if item.status == "Done"]
+        return self._find_items_by_status("Done")
+
+    def _find_items_by_status(self, status):
+        return [item for item in self._items if item.status == status]
